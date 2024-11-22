26183e12063e00031b8341784b8061407e750ce5
dimitri <dyaryarashvili@gmail.com>
2024-11-22 20:51:07 +0400

    Feat: fixes

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -28,7 +28,7 @@ const Explore = () => {
   }
 
   const shouldShowSearchResults = searchValue !== '';
-  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item.documents.length === 0);
+  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item?.documents.length === 0);
   
   return (
     <div className="explore-container">
@@ -50,15 +50,15 @@ const Explore = () => {
       </div>
 
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
-        {shouldShowSearchResults ? (
+        {shouldShowSearchResults && searchedPosts ? (
           <SearchResults 
            isSearchFetching={isSearchFetching}
-           searchedPosts={searchedPosts}
+           searchedPosts={searchedPosts.documents}
           /> 
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : posts.pages.map((item, index) =>(
-          <GridPostList key={`page-${index}`} posts={item.documents} />
+          <GridPostList key={`page-${index}`} posts={item?.documents} />
         ))}
       </div>
 
